291b851a0a8dc092ea0550a27ab3dc07b931ecd6
dongguacute <gudupao2022@gmail.com>
2025-03-05 20:28:29 +0800

    更新阅读增强

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vitepress'
 import footnote from 'markdown-it-footnote'
 export default defineConfig({
+  vite: { 
+    optimizeDeps: {
+      exclude: [ 
+        '@nolebase/vitepress-plugin-enhanced-readabilities/client', 
+        'vitepress', 
+        '@nolebase/ui', 
+      ], 
+    },
+    ssr: { 
+      noExternal: [ 
+        // 如果还有别的依赖需要添加的话，并排填写和配置到这里即可 //
+        '@nolebase/vitepress-plugin-enhanced-readabilities', 
+        '@nolebase/ui', 
+      ], 
+    }, 
+  }, 
   locales: {
     root: {
       label: '中文',
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,24 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme as ThemeConfig } from 'vitepress'
+import { h } from 'vue'
 import './style.css'
-export default DefaultTheme
+import { 
+    NolebaseEnhancedReadabilitiesMenu, 
+    NolebaseEnhancedReadabilitiesScreenMenu, 
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
+export const Theme: ThemeConfig = {
+    extends: DefaultTheme,
+    Layout: () => {
+      return h(DefaultTheme.Layout, null, {
+        // 为较宽的屏幕的导航栏添加阅读增强菜单
+        'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
+        // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+        'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu), 
+      })
+    },
+    enhanceApp() {
+      // 其他的配置...
+    },
+}
+export default Theme
